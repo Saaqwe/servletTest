611b4d042ed1d6426bf088d14416f1be34017d53
Saaqwe <timurahaha@gmail.com>
2021-09-05 22:40:07 +0400

    PJ-2

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -3,6 +3,6 @@ public class Servlet {
         System.out.println("not hi");
     }
     private void sayBye(){
-        System.out.println("Not bye");
+        System.out.println("sadksd");
     }
 }
